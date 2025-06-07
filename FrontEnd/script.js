cd8c5829c1edab99da4f2870abdf9030e281ce54
Feihung Mayer <mayerfeihung@gmail.com>
2025-06-07 11:49:59 +0200

    Création de Methodes  JS pour récup dynamiquement les works et les catégories

---
+++ FrontEnd/script.js
@@ -0,0 +1,75 @@
+const gallery = document.querySelector('.gallery');
+const categories = document.querySelector('.categories');
+
+const BASE_URL = 'http://localhost:5678';
+
+let worksFetched = []; 
+let categoriesFetched = [];
+
+export const fetchWorks = async () => {
+    try {
+        let response = await fetch(`${BASE_URL}/api/works`);
+        // Gérer les erreurs API
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        let works = await response.json();
+        //Vérifier si le tableau est vide au départ
+        worksFetched.length = 0;
+        worksFetched = works;
+        console.log("Ensemble des works",worksFetched);
+        for (let work of worksFetched) {
+            const figure = createFigure(work);
+            gallery.appendChild(figure);
+        }
+    } catch (error) {
+        console.error('Error fetching works:', error);
+    }
+}
+
+fetchWorks();
+
+const createFigure = (work) => {
+    const figure = document.createElement('figure');
+    const img = document.createElement('img');
+    img.src = work.imageUrl;
+    img.alt = work.title;
+    figure.appendChild(img);
+    const figcaption = document.createElement('figcaption');
+    figcaption.textContent = work.title;
+    figure.appendChild(figcaption);
+    return figure;
+}
+
+// Fonction pour récupérer les catégories
+export const fetchCategories = async () => {
+    try {
+        let response = await fetch(`${BASE_URL}/api/categories`);
+        // Gérer les erreurs API
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        let categories = await response.json();
+        categories.unshift({ id: 0, name: 'Tous' }); // Ajouter la catégorie "Tous" au début du tableau
+        //Vérifier si le tableau est vide au départ
+        categoriesFetched.length = 0;
+        console.log("Ensemble des catégories", categories);
+        categoriesFetched = categories;
+        createCategoryButtons(); // Appeler la fonction pour créer les boutons de catégorie
+    } catch (error) {
+        console.error('Error fetching categories:', error);
+    }
+}
+fetchCategories();
+
+// Fonction pour créer les boutons de catégorie
+export const createCategoryButtons = () => {
+    categories.innerHTML = ''; // Vider les catégories existantes
+    for (let category of categoriesFetched) {
+        const button = document.createElement('button');
+        button.textContent = category.name;
+        button.setAttribute('data-category-id', category.id); // Ajouter l'ID de la catégorie comme attribut de données
+        button.classList.add('category-button');
+        categories.appendChild(button);
+        }
+}
