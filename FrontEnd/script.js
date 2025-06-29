2e32b50bb1df96008181d52f401727bd71bb3978
Feihung Mayer <mayerfeihung@gmail.com>
2025-06-29 14:19:31 +0200

    Initialisation de la modale

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -113,3 +113,13 @@ if (token) {
 }
 
 
+const modalContainer = document.querySelector('.modal-container');
+
+const modalTriggers = document.querySelectorAll('.modal-trigger');
+
+modalTriggers.forEach(trigger => trigger.addEventListener('click', toggleModal));
+
+function toggleModal() {
+  modalContainer.classList.toggle('active');
+
+}
